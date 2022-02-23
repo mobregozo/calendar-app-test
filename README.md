28c2515313faf963e9e030972ab9ef220ae98ef9
mobregozo <obregozo.manuel@gmail.com>
2022-02-23 18:40:47 +0100

    initial version

---
+++ README.md
@@ -0,0 +1,5 @@
+## Calendar App
+
+- I have created a simplified version of the calendar App, which allows the user to mark busy days.
+- Of course appoints should consider start and end date time, and as for now edition and delition is not something I was able to cover.
+- Just an alert that tells the user when a day was already taken.
---
+++ js/calendar.js
@@ -0,0 +1,43 @@
+let events = [];
+
+const showError = () => {
+  alert("There is another event in that particular day");
+};
+
+const addEvents = (event) => {
+  // Check if the event is coming from a day
+  if (![...event.target.classList].includes("day")) return;
+  const day = event.target.innerText;
+  if (events.includes(day)) {
+    showError();
+    return;
+  }
+  events.push(day);
+  event.target.classList.add("selected");
+};
+
+const createCalendar = () => {
+  const calendarBox = document.querySelector("#calendar");
+  calendarBox.addEventListener("click", addEvents);
+  const node = document.createElement("div");
+  node.classList.add("container");
+  const currentDate = new Date();
+  const totalDaysOfTheMonth = new Date(
+    currentDate.getFullYear(),
+    currentDate.getMonth(),
+    0
+  ).getDate();
+
+  for (let day = 1; day <= totalDaysOfTheMonth; day++) {
+    let newDay = document.createElement("div");
+    newDay.innerHTML = `
+        <div class="day" >    
+            ${day} 
+        </div>
+    `;
+    node.appendChild(newDay);
+  }
+  calendarBox.appendChild(node);
+};
+
+createCalendar();
